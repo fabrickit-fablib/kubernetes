9cf7335454c428f98635daed3fa5891ef9d169d4
Shunya Kitada <syun.kitada@gmail.com>
2016-12-10 23:04:40 +0900

    add calico

--- kubernetes.py
+++ kubernetes.py
@@ -14,7 +14,6 @@ class Kubernetes(SimpleBase):
                 'vim',
                 'wget',
                 'kubernetes',
-                'flannel',
             ]
         }
 
@@ -23,16 +22,26 @@ class Kubernetes(SimpleBase):
                 'kube-proxy',
                 'kubelet',
                 'docker',
-                'flanneld',
             ]
         }
 
     def init_before(self):
-        if env.host == env.cluster['kubernetes']['kube_master']:
+        data = env.cluster[self.data_key]
+        if env.host == data['kube_master']:
             self.services['CentOS .*'].extend([
                 'kube-apiserver',
                 'kube-controller-manager',
                 'kube-scheduler',
+            ])
+
+        self.network = data['network']
+        if self.network['type'] == 'flannel':
+            self.services['CentOS .*'].extend([
+                'flanneld'
+            ])
+
+            self.packages['CentOS .*'].extend([
+                'flannel'
             ])
 
     def init_after(self):
@@ -63,12 +72,39 @@ class Kubernetes(SimpleBase):
             self.handlers['restart_kube-kubelet'] = True
 
         if env.host == env.cluster['kubernetes']['kube_master']:
-            filer.template('/tmp/flannel.json')
-            run('etcdctl set /atomic.io/network/config < /tmp/flannel.json')
             filer.template('/etc/kubernetes/serviceaccount.key')
 
-        if filer.template('/etc/sysconfig/flanneld', data=data):
-            self.handlers['restart_flanneld']
-
+        self.setup_network()
         self.start_services().enable_services()
         self.exec_handlers()
+
+    def setup_network(self):
+        data = self.init()
+        if self.network['type'] == 'flannel':
+            if env.host == env.cluster['kubernetes']['kube_master']:
+                filer.template('/tmp/flannel.json')
+                run('etcdctl set /atomic.io/network/config < /tmp/flannel.json')
+
+            if filer.template('/etc/sysconfig/flanneld', data=data):
+                self.handlers['restart_flanneld'] = True
+
+        elif self.network['type'] == 'calico':
+            with api.warn_only():
+                result = sudo('calicoctl node show | grep {0}'.format(
+                    env.node['ip']['default_dev']['ip']))
+
+            if result.return_code != 0:
+                with api.cd('/tmp'):
+                    sudo('wget https://github.com/projectcalico/calico-containers/releases/download/v0.19.0/calicoctl')  # noqa
+                    sudo('chmod 755 calicoctl')
+                    sudo('mv calicoctl /usr/bin/')
+                    sudo('wget https://github.com/projectcalico/calico-cni/releases/download/v1.3.0/calico')  # noqa
+                    sudo('chmod 755 calico')
+                    sudo('mv calico /usr/bin/')
+
+                sudo('ETCD_AUTHORITY=127.0.0.1:2379; calicoctl node --ip={0}'.format(
+                    env.node['ip']['default_dev']['ip']))
+                sudo('calicoctl node show')
+
+            filer.mkdir('/etc/cni/net.d')
+            filer.template('/etc/cni/net.d/10-calico.conf', data=data)
